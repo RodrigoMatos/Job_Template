2d29701704162de7970c18a038075c65515e354f
Rodrigo Matos <romatos@indracompany.com>
2016-08-25 16:09:53 -0300

    Add funcionalidade de ler XML

--- src/exemplos/XMLExemplos.java
+++ src/exemplos/XMLExemplos.java
@@ -1,5 +1,9 @@
 package exemplos;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import org.jdom2.Element;
 
 import arquivos.xml.ArquivoXML;
@@ -9,8 +13,18 @@ public class XMLExemplos {
 	public void lerArquivoXML() {
 		
 		try {
-			Element arquivoXML = ArquivoXML.lerXML("D:\\teste\\teste.xml");
+			Element arquivoXML = ArquivoXML.lerXML("D:\\testes\\teste.xml");
+			List elements = ArquivoXML.getFilhos(arquivoXML, "ELEMENT");
 			
+			Iterator i = elements.iterator();
+			Map<String,String> filho;			
+			while (i.hasNext()) {
+				Element element = (Element) i.next();
+				filho = ArquivoXML.getConteudoDoFilho(element);
+				System.out.println("Codigo: " + filho.get("CODIGO"));
+				System.out.println("Descrição: " + filho.get("DESCRICAO"));
+				System.out.println();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,21 @@
 package main;
 
-import exemplos.ArquivoExcelExemplos;
+import exemplos.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		/*
 		ArquivoExcelExemplos ex = new ArquivoExcelExemplos();
-//		ex.descompactarArquivoZIPPara();
-		
 		ex.criarExcel();
 		ex.alterarExcel();
+		 */
+		
+		XMLExemplos ex = new XMLExemplos();
+		
+		ex.lerArquivoXML();
+		
 	}
 
 }
