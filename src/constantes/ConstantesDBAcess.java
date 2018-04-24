88a15b8de8de1e27a8264dcf9e84db87c2b5d81a
Rodrigo.Matos <Rodrigo.Matos@TOPOSNT17.topos.local>
2018-04-24 10:34:25 -0300

    AJUSTES

--- src/constantes/ConstantesDBAcess.java
+++ src/constantes/ConstantesDBAcess.java
@@ -10,9 +10,9 @@ import model.BancoDadosVO;
 public abstract class ConstantesDBAcess {
 
 	public static final BancoDadosVO BANCOSCIENCEBCV = new BancoDadosVO
-			( "jdbc:oracle:thin:@10.240.2.12:1521:SMAPBCV"
-			, "usr_science"
-			, "PKNGR"
+			( "jdbc:oracle:thin:@URL_SERVIDOR:PORTA/SID"
+			, "usuario"
+			, "senha"
 			, "oracle.jdbc.driver.OracleDriver"
 			, 1
 			, 1
